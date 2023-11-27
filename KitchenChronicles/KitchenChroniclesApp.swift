25265021f367de03307fc155b86c42c01391fba5
Istionia <timmoyoong678@protonmail.com>
2023-11-27 23:26:13 +0800

    Initial Commit

---
+++ KitchenChronicles/KitchenChroniclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenChroniclesApp.swift
+//  KitchenChronicles
+//
+//  Created by Tim Yoong on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenChroniclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
