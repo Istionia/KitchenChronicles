d2e834698b183636adb3b74bfd5adeaf6ea383fa
Istionia <timmoyoong678@protonmail.com>
2023-12-04 15:03:19 +0800

    Focus on func createSampleData() to... well, do exactly that.

--- KitchenChronicles/DataController.swift
+++ KitchenChronicles/DataController.swift
@@ -33,10 +33,12 @@ enum Meals: String, CaseIterable {
 }
 
 enum DietPreferences: String, CaseIterable {
-    case lactoseIntolerant, glutenSensitive, vegetarian, plantBased, kosher, keto, diabetic, dairyFree, lowCarb
+    case none, lactoseIntolerant, glutenSensitive, vegetarian, plantBased, kosher, keto, diabetic, dairyFree, lowCarb
     
     var description: String {
         switch self {
+        case .none:
+            return "None"
         case .lactoseIntolerant:
             return "Lactose Intolerant"
         case .glutenSensitive:
@@ -60,10 +62,12 @@ enum DietPreferences: String, CaseIterable {
 }
 
 enum FoodAllergies: String, CaseIterable {
-    case milk, eggs, fish, crustacean, treeNuts, peanuts, wheat, soybean
+    case none, milk, eggs, fish, crustacean, treeNuts, peanuts, wheat, soybean
     
     var description: String {
         switch self {
+        case .none:
+            return "None"
         case .milk:
             return "Milk"
         case .eggs:
@@ -204,15 +208,39 @@ class DataController: ObservableObject {
         let recipe = Recipe(context: viewContext)
         recipe.id = UUID()
         recipe.name = "Cantonese Poached Chicken (Bai Qie Ji)"
-        recipe.ingredients
-        recipe.instructions
+        
+        let ingredient = Ingredient(context: viewContext)
+        recipe.ingredients = [
+            ingredient.name = "3-4 pound whole chicken (ideally organic free-range; optional head and feet still on; at room temperature)",
+            ingredient.name = "2 scallions",
+            ingredient.name = "5 slices ginger",
+            ingredient.name = "3 tablespoons scallions (finely minced, white and light green parts only)",
+            ingredient.name = "2 tablespoons ginger (finely minced)",
+            ingredient.name = "3 tablespoons oil",
+            ingredient.name = "salt (to taste)",
+            ingredient.name = "soy sauce (optional); replace with coconut aminos or tamari sauce if concerned about allergies or gluten intolerance"
+        ]
+        
+        let instruction = Instruction(context: viewContext)
+        recipe.instructions = [
+            instruction.step = "1. Make sure your chicken is at room temperature (trying to poach a cold chicken right out of the refrigerator will result in uneven cooking or undercooking). Clean the chicken by rinsing it under cold water, paying special attention to the cavity. Any giblets should already be removed but there may still be organs on the inside that should be removed or sometimes stray feathers that need to be plucked. Experts recommend that it's not necessary to wash your chicken before cutting and cooking but in this case for a whole chicken (especially if you get it from a live poultry place or even from Asian markets), it's a step that shouldn't be skipped, in my opinion.  When washing and prepping the whole chicken, be very careful about splashing water and contaminating surfaces with unwanted bacteria. Be careful not to break or trim away any of the skin on the chicken, as you don’t want the meat exposed to the boiling water as it cooks. This will ensure a moist, silky texture in the final product.",
+            instruction.step = "2. Fill a large stock pot with water, just enough to submerge the chicken completely. You can determine this by putting the entire chicken in the pot, filling it with water until the chicken is submerged, and then removing the chicken. Do not turn on the heat while the chicken is still in the pot! This method of ensuring you have just enough water to submerge the chicken (and avoiding any extra) will ensure you have a more flavorful stock to save at the end. We used about 18 cups of water to submerge a 4 pound chicken in a deep stock pot.",
+            instruction.step = "3. Once you have your water properly measured into the pot, add 2 scallions and 5 slices of ginger, and bring it to a boil. Once boiling, slowly lower the chicken into the pot, legs down and head up. It’s ok if the breast is peeking out of the water a bit. The water will cool down and stop boiling when you add the chicken, so bring it up to a boil once again, and do not walk away from the pot.",
+            instruction.step = "4. Once the water boils again, IMMEDIATELY lift the chicken out of the water very carefully. You can carefully hook two wooden spoons under the wings to lift the chicken up. The goal is to empty any colder water that may be trapped inside the cavity. Once you’ve released that water, lower the chicken back into the pot, and bring to a boil again.",
+            instruction.step = "5. When the water is JUST starting to boil, turn the heat down. Keep it at barely a simmer. There should be very little movement in the water, but it also shouldn’t be still. Cover the pot, and keep the heat around the lowest setting so the liquid continues to simmer slowly. Cook for about 35-40 minutes, roughly 10-11 minutes per pound. Depending on the size of your chicken, it may take more or less time to cook it through. You can check to make sure the water is bubbling slowly/gently and not boiling too vigorously, but try to avoid uncovering the pot while it’s cooking.",
+            instruction.step = "6. Poke a chopstick or skewer into the thigh to check for doneness. If the juices run clear, it's done. Carefully lift the chicken out of the pot and transfer it to a large bowl of ice water. Cool completely.",
+            instruction.step = "7. While the chicken is cooling, make the sauce. You have the option to make two versions—one with just scallions, ginger, oil and salt, and one with soy sauce. The plain version is more traditional, as it really lets the flavors of chicken, ginger, and scallion shine through. Judy loves to add soy sauce, and it’s also a tasty option! Start with the plain version, and then scoop some of it out into another bowl and add soy sauce. Try both and see which your loved ones like best!",
+            instruction.step = "8. When the chicken is out of the ice water, you can brush it lightly with oil or some of the fat floating atop the poaching liquid to give it that enticing, shiny look!",
+            instruction.step = "9. To serve, carve your chicken into pieces that you can easily grab with chopsticks. For directions on the traditional Chinese way to cut up said chicken, follow this link: https://thewoksoflife.com/how-to-cut-whole-chicken-chinese/. Serve with your sauce(s) and some steamed rice."
+        ]
+        
         recipe.entryDate = .now
-        recipe.meal
-        recipe.dietPreferences
-        recipe.foodAllergies
-        recipe.cuisine
-        recipe.occasion
-        recipe.servingSize
+        recipe.meal = Meals.dinner.rawValue
+        recipe.dietPreferences = DietPreferences.none.rawValue
+        recipe.foodAllergies = FoodAllergies.none.rawValue
+        recipe.cuisine = Cuisines.chinese.rawValue
+        recipe.occasion = Occasions.chineseNewYear.rawValue
+        recipe.servingSize = ServingSizes.family.rawValue
         
         try? viewContext.save()
     }
